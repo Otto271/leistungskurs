1bce2e4373b2e0c91292c45120d9c48e5f2ee82a
Otto271 <julius.hillenbrand@gmx.de>
2025-09-06 23:00:00 +0200

    blatt03 änderungen

--- Schule/src/blatt03/MitOderOhneStrom.java
+++ Schule/src/blatt03/MitOderOhneStrom.java
@@ -11,9 +11,9 @@ public class MitOderOhneStrom {
             System.out.println("Befindet sich auf dem Eingang Strom? (true/false)");
             boolean not_eingang = input.nextBoolean();
             if (not_eingang) {
-                System.out.println(not_eingang + " = " + !not_eingang);
+                System.out.println(true + " = " + false);
             } else if (!not_eingang) {
-                System.out.println(!not_eingang + " = " + not_eingang);
+                System.out.println(false + " = " + true);
             }
         }
         if (gatter.equals("OR")) {
@@ -25,13 +25,22 @@ public class MitOderOhneStrom {
             boolean or_eingang_2 = input.nextBoolean();
             if (or_eingang_1 || or_eingang_2) {
                 System.out.println(or_eingang_1 + " AND " + or_eingang_2 +  " = true");
-            }else {
-                System.out.println(or_eingang_1 + " AND " + or_eingang_2 +  " = false");
+            }else if (!or_eingang_1 || !or_eingang_2){
+                System.out.println(false + " AND " + false + " = false");
             }
         }
         if (gatter.equals("AND")) {
             System.out.println("Das AND-Gatter:");
             System.out.println("Verfügt über zwei Eingänge, Ergebnis true wenn beide true sind.");
+            System.out.println("Befindet sich auf dem ersten Eingang Strom? (true/false)");
+            boolean and_eingang_1 = input.nextBoolean();
+            System.out.println("Befindet sich auf dem zweiten Eingang Strom? (true/false)");
+            boolean and_eingang_2 = input.nextBoolean();
+            if (and_eingang_1 && and_eingang_2) {
+                System.out.println(true + " AND" + true + " = true");
+            }else if (!and_eingang_1 && !and_eingang_2) {
+                System.out.println(and_eingang_1 + " AND " + and_eingang_2 +  " = false");
+            }
         }
         if (gatter.equals("XOR")) {
             System.out.println("Das XOR-Gatter:");

0c45271be1421e44d5ab314d01eb0c658251c6a3
Otto271 <julius.hillenbrand@gmx.de>
2025-09-28 18:15:54 +0200

    Blatt06

--- Schule/src/blatt06/Fibonacci.java
+++ Schule/src/blatt06/Fibonacci.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Wie Lang soll n sein!");
+        System.out.println("Wie viele Zahlen sollen ausgegeben werden?");
         int n = input.nextInt();
         int[] fibonacci = new int[n];
         fibonacci[0] = 0;
         fibonacci[1] = 1;
-        System.out.print(fibonacci[0] + " " + fibonacci[1] + " ");
-        for (int i = 2; i < fibonacci.length; i++) {
+        System.out.println(fibonacci[0] + "\n" + fibonacci[1]);
+        for (int i = 2; i < n; i++) {
             fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-            System.out.print(fibonacci[i] + " ");
+            System.out.println(fibonacci[i]);
         }
-        int quo = fibonacci[n--] / fibonacci[n - 2];
+        double q1 = fibonacci[n - 1];
+        double q2 = fibonacci[n - 2];
+        double quo = q1 / q2; //Umso höher n umso näher geht der Quotient an 1,618
         System.out.println("Quotient: " + quo);
     }
 }

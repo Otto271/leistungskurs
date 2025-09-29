00d3bf12585e7fc6210f8537afa8473d1f8cd62b
JuliusHillenbrand <julius.hillenbrand@gmx.de>
2025-09-29 09:30:35 +0200

    Arbeit an Blatt07

---
+++ Schule/src/blatt07/ArbeitMitArrays.java
@@ -0,0 +1,45 @@
+package blatt07;
+
+public class ArbeitMitArrays {
+    public static void printArray(int[] arr) {
+        System.out.print("[ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " | ");
+        }
+        System.out.println(" ]");
+    }
+    public static void printArray(double[] arr) {
+        System.out.print("[ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " | ");
+        }
+        System.out.println(" ]");
+    }
+    public static void printArray(boolean[] arr) {
+        System.out.println("[ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " | ");
+        }
+        System.out.println(" ]");
+    }
+    public static void printArray(char[] arr) {
+        System.out.println("[ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " | ");
+        }
+        System.out.println(" ]");
+    }
+    public static void printArray(String[] arr) {
+        System.out.println("[ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " | ");
+        }
+        System.out.println(" ]");
+    }
+    public static int istSortiert(int a, int b){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}

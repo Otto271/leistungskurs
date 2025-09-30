60f16fa640f0dd7de3d808e320a3bc9a09349390
JuliusHillenbrand <julius.hillenbrand@gmx.de>
2025-09-30 13:02:00 +0200

    Blatt07

--- Schule/src/blatt07/ArbeitMitArrays.java
+++ Schule/src/blatt07/ArbeitMitArrays.java
@@ -36,8 +36,65 @@ public class ArbeitMitArrays {
         }
         System.out.println(" ]");
     }
-    public static int istSortiert(int a, int b){
-
+    public static boolean istSortiert(int[] arr){
+        boolean a = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                a = false;
+                break;
+            } else {
+                a = true;
+            }
+        }
+        return a;
+    }
+    public static int[] addieren(int[] arr_1, int[] arr_2) {
+        int[] arr_3 = new int[arr_1.length];
+        if (arr_1.length < arr_2.length){
+            arr_3 = new int[arr_2.length];
+            for (int i = 0; i < arr_2.length; i++) {
+                if (i != arr_1.length) {
+                    arr_3[i] = arr_1[i] + arr_2[i];
+                } else {
+                    arr_3[i] = arr_2[i];
+                }
+            }
+        } else if (arr_2.length < arr_1.length) {
+            for (int i = 0; i < arr_1.length; i++) {
+                if (i != arr_2.length) {
+                    arr_3[i] = arr_2[i] + arr_1[i];
+                } else {
+                    arr_3[i] = arr_2[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < arr_1.length; i++) {
+                arr_3[i] = arr_1[i] + arr_2[i];
+            }
+        }
+        return arr_3;
+    }
+    public static int[] shiftLeft(int[] arr){
+        int a = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i++];
+        }
+        arr[arr.length - 1] = a;
+        return arr;
+    }
+    public static int[] shiftLeftAbsolute(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i++];
+        }
+        arr[arr.length - 1] = 0;
+        return arr;
+    }
+    public static int[] shiftRightAbsolute(int[] arr){
+        for (int i = arr.length; i > 0; i--) {
+            arr[i] = arr[i--];
+        }
+        arr[0] = 0;
+        return arr;
     }
     public static void main(String[] args) {
 
---
+++ Schule/src/blatt07/Stein.java
@@ -0,0 +1,28 @@
+package blatt07;
+
+public class Stein {
+    public static int steinGGT(int a, int b){
+        int k = 0;
+        if (a % 2 == 0 && b % 2 == 0) {
+            while(a != 0 || b != 0){
+                    a /= 2;
+                    b /= 2;
+                    k++;
+            }
+        } else if (a % 2 == 0) {
+            while(a != 0){
+                a /= 2;
+                k++;
+            }
+        } else if (b % 2 == 0) {
+            while(b != 0){
+                b /= 2;
+                k++;
+            }
+        } else {
+            while(a != 0 || b != 0){
+                if (a > b){}
+            }
+        }
+    }
+}

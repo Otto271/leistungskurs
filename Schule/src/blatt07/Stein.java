43b120008c831cd94a6a1aa778adfa83a3ab763b
Otto271 <julius.hillenbrand@gmx.de>
2025-10-04 15:31:26 +0200

    Blatt08

--- Schule/src/blatt07/Stein.java
+++ Schule/src/blatt07/Stein.java
@@ -1,29 +1,33 @@
-/*package blatt07;
+package blatt07;
 
 public class Stein {
-    public static int steinGGT(int a, int b){
+    public static double steinGGT(int a, int b){
         int k = 0;
-        if (a % 2 == 0 && b % 2 == 0) {
-            while(a != 0 || b != 0){
-                    a /= 2;
-                    b /= 2;
-                    k++;
-            }
-        } else if (a % 2 == 0) {
-            while(a != 0){
+        double ggt = 0;
+        while (a != 0 || b != 0) {
+            if (a % 2 == 0 && b % 2 == 0) {
                 a /= 2;
-                k++;
-            }
-        } else if (b % 2 == 0) {
-            while(b != 0){
                 b /= 2;
                 k++;
-            }
-        } else {
-            while(a != 0 || b != 0){
-                if (a > b){}
+            } else if (a % 2 == 0) {
+                a /= 2;
+                k++;
+            } else if (b % 2 == 0) {
+                b /= 2;
+                k++;
+            } else if (a >= b){
+                a = a - b / 2;
+                k++;
+            } else {
+                b = b - a / 2;
+                k++;
             }
         }
+        if (a == 0){
+            ggt = b * Math.pow(2, k);
+        } else {
+            ggt = a * Math.pow(2, k);
+        }
+        return ggt;
     }
 }
-*/

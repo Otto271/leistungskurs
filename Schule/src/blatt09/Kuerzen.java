ad73c57c58a8d6739a508e8f227a0c7fa5316f12
JuliusHillenbrand <julius.hillenbrand@gmx.de>
2025-11-04 13:05:37 +0100

    Blatt09

--- Schule/src/blatt09/Kuerzen.java
+++ Schule/src/blatt09/Kuerzen.java
@@ -2,6 +2,13 @@ package blatt09;
 
 public class Kuerzen {
     public static boolean istKuerzbar(int a, int b) {
+        if (a > b) {
+            for (int i = 1; i <= a; i++) {
 
+            }
+            for (int i = 1; i <= b; i++) {
+
+            }
+        }
     }
 }

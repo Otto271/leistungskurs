dad3e798309fedb13eee48f87dbdc586ecf76036
JuliusHillenbrand <julius.hillenbrand@gmx.de>
2025-11-05 11:20:21 +0100

    Blatt09

--- Schule/src/blatt09/Kuerzen.java
+++ Schule/src/blatt09/Kuerzen.java
@@ -3,12 +3,41 @@ package blatt09;
 public class Kuerzen {
     public static boolean istKuerzbar(int a, int b) {
         if (a > b) {
-            for (int i = 1; i <= a; i++) {
-
+            for (int i = 2; i <= a; i++) {
+                if (a % i == 0 && b % i == 0) {
+                    return true;
+                }
             }
-            for (int i = 1; i <= b; i++) {
-
+        } else if (a < b) {
+            for  (int i = 2; i <= b; i++) {
+                if (a % i == 0 && b % i == 0) {
+                    return true;
+                }
             }
         }
+        return false;
+    }
+    public static void kuerzen(int a, int b) {
+        while (true) {
+            if (istKuerzbar(a, b)) {
+                if (a > b) {
+                    for (int i = 2; i <= a; i++) {
+                        a /= i;
+                        b /= i;
+                    }
+                } else if (a < b) {
+                    for (int i = 2; i <= b; i++) {
+                        a /= i;
+                        b /= i;
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+        System.out.println(a + "/" + b);
+    }
+    public static void main(String[] args) {
+        kuerzen(2, 4);
     }
 }
---
+++ Schule/src/blatt09/Primzahlen.java
@@ -0,0 +1,57 @@
+package blatt09;
+
+import java.util.Arrays;
+
+public class Primzahlen {
+    public static boolean istPrim(int a) {
+        if (a == 1) {
+            return false;
+        }
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printPrim(int a) {
+        for (int i = 1; i < a; i++) {
+            if (istPrim(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static int[] generierePrimzahlen(int n) {
+        int j = 0;
+        int i = 1;
+        int[] arr = new int[n];
+        while (true){
+            if (istPrim(i)) {
+                arr[j] = i;
+                j++;
+            } else if (j == n) {
+                break;
+            }
+            i++;
+        }
+        return arr;
+    }
+
+    public static void primfaktorzerlegung(int n) {
+        int[] arr = generierePrimzahlen(n);
+        int[] arr_2 = new int[n];
+        int i = 0;
+        while (true) {
+            if (n %  arr[i] == 0) {
+                arr_2[i] = arr[i];
+            }
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = generierePrimzahlen(10);
+        System.out.println(Arrays.toString(arr));
+    }
+}

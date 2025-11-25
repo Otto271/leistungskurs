fd2dff167e845e0394b10c58c5f4d7887a2830e9
JuliusHillenbrand <julius.hillenbrand@gmx.de>
2025-11-25 13:05:17 +0100

    Blatt12/13

--- Schule/src/blatt12/InsertionSort.java
+++ Schule/src/blatt12/InsertionSort.java
@@ -2,6 +2,30 @@ package blatt12;
 
 public class InsertionSort {
     public static int[] insertionSort(int[] arr) {
-        return null;
+        for (int i = 0; i < arr.length-1; i++) {
+            int wert =  arr[i];
+            int j = i;
+            while (j > 0 && arr[j-1] > wert){
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = wert;
+        }
+        return arr;
+    }
+    public static void insertionSortInPlace(int[] arr) {
+        int[] temp = new int[arr.length];
+        int j = 0;
+        int n = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int k = 1; k < arr.length - 1; k++) {
+                if (arr[k] < arr[k - 1]) {
+                    j = arr[k];
+                    n = k;
+                }
+            }
+            arr[n] = 0;
+            temp[i] = j;
+        }
     }
 }
---
+++ Schule/src/blatt12/SelectionSort.java
@@ -0,0 +1,28 @@
+package blatt12;
+
+public class SelectionSort {
+    public static void SelectionSort(int[] arr){
+        int[] temp = new int[arr.length];
+        int i = 1;
+        int j = 0;
+        int n = 0;
+        while (i < arr.length) {
+            int k = i;
+            while (k < arr.length) {
+                if (arr[k] < arr[k - 1]) {
+                    j = arr[k];
+                    n = k;
+                }
+                k++;
+            }
+            arr[n] = 999999999;
+            temp[i--] = j;
+            System.out.println(arr[i--]);
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[] {64, 25, 12, 22, 11};
+        SelectionSort(arr);
+    }
+}

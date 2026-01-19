34d076721acc459e292e9a0bbdbab6b7c20fb083
JuliusHillenbrand <julius.hillenbrand@gmx.de>
2026-01-19 09:30:10 +0100

    Blatt14/15

--- Schule/src/blatt15/Farben.java
+++ Schule/src/blatt15/Farben.java
@@ -2,6 +2,37 @@ package blatt15;
 import schisch_visualizer.*;
 import blatt13.Zufall;
 public class Farben {
+    public static int zaehlen(int teamnummer,char[][] arr){
+        int count = 0;
+        if (teamnummer == 7) {
+            for  (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] == '7') {
+                        count++;
+                    }
+                }
+            }
+        } else if (teamnummer == 9) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] == '9') {
+                        count++;
+                    } else if (arr[i][j] == 'P') {
+                        
+                    }
+                }
+            }
+        } else if (teamnummer == 0) {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    if (arr[i][j] == ' ') {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
     public static void initialisiereSpielfeld(char[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
@@ -15,19 +46,53 @@ public class Farben {
             }
         }
     }
-    public static void startPosition(char[][] arr) {
-        for (int i = 8; i > 0; i--) {
-            arr[Zufall.zufallGanz(1, 40)][Zufall.zufallGanz(1, arr.length-1)] = 'P';
+    public static void startPosition(char[][] arr, int[] posx, int[] posy) {
+        for (int i = 4; i > 0; i--) {
+            int zufallx = Zufall.zufallGanz(1, arr.length/2);
+            int zufally = Zufall.zufallGanz(1, arr.length-1);
+            arr[zufallx][zufally] = 'P';
+            posx[i] = zufallx;
+            posy[i] = zufally;
         }
-        for (int i = 8; i > 0; i--) {
-            arr[Zufall.zufallGanz(41,arr.length-1)][Zufall.zufallGanz(1,arr.length-1)] = 'P';
+        for (int i = 4; i > 0; i--) {
+            int zufallx = Zufall.zufallGanz(arr.length/2+1, arr.length-1);
+            int zufally = Zufall.zufallGanz(1, arr.length-1);
+            arr[zufallx][zufally] = 'P';
+            posx[i] = zufallx;
+            posy[i] = zufally;
         }
+    }
+    public static int[] positionx(char[][] arr, int[] posx) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 'P') {
+                    posx[count] = i;
+                    count++;
+                }
+            }
+        }
+        return posx;
+    }
+    public static int[] positiony(char[][] arr, int[] posy) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 'P') {
+                    posy[count] = i;
+                    count++;
+                }
+            }
+        }
+        return posy;
     }
     public static void main(String[] args) {
         SchischVisualizer sv = new SchischVisualizer();
-        char[][] arr = new char[40][40];
+        char[][] arr = new char[80][80];
+        int[] posx = new int[8];
+        int[] posy = new int[8];
         initialisiereSpielfeld(arr);
-        startPosition(arr);
+        startPosition(arr, posx, posy);
         sv.step(arr);
         sv.start();
     }

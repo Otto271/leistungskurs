4f92314c727279462f6906e5911474b880691c89
JuliusHillenbrand <julius.hillenbrand@gmx.de>
2026-01-28 11:17:52 +0100

    Blatt15

--- Schule/src/blatt15/Spiegel.java
+++ Schule/src/blatt15/Spiegel.java
@@ -1,56 +1,62 @@
 package blatt15;
-import blatt14.MultiArrays;
 import schisch_visualizer.*;
 import blatt13.Zufall;
 
 public class Spiegel {
     static char[][] spielfeld = new char[40][40];
-    public static void print2DArraySpiegel(char[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print("[ ");
-            for (int j = 0; j < arr[i].length - 1; j++) {
-                System.out.print(arr[i][j] + " ");
+    public static void zufallSpiegel() {
+        for (int i = 0; i < spielfeld.length; i++) {
+            for (int j = 0; j < spielfeld[i].length; j++) {
+                if(i == 0 || i == spielfeld.length-1) {
+                    spielfeld[i][j] = '8';
+                } else if (j == 0 || j == spielfeld[i].length-1) {
+                    spielfeld[i][j] = '8';
+                } else {
+                    spielfeld[i][j] = ' ';
+                }
             }
-            System.out.print(arr[i][arr[i].length - 1]);
-            System.out.println(" ]");
         }
-    }
-    public static void zufallSpiegel(char[][] arr) {
-        Farben.initialisiereSpielfeld();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                int zufall = Zufall.zufallGanz(1,4);
-                if (arr[i][j] == ' ') {
-                    if (zufall == 1) {
-                        arr[i][j] = '/';
-                    } else if (zufall == 2) {
-                        arr[i][j] = '\\';
+        for (int i = 0; i < spielfeld.length; i++) {
+            for (int j = 0; j < spielfeld[i].length; j++) {
+                int zufall = Zufall.zufallGanz(1,3);
+                if (spielfeld[i][j] == ' ') {
+                    if (Math.random() <= 0.3) {
+                        if (zufall == 1) {
+                            spielfeld[i][j] = '/';
+                        } else if (zufall == 2) {
+                            spielfeld[i][j] = '\\';
+                        }
                     }
                 }
             }
         }
     }
-    public static void zufallZiele(char[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] != '/' && arr[i][j] != '\\' && arr[i][j] != '8') {
-                    if (Zufall.zufallGanz(1, 3) == 1) {
-                        arr[i][j] = 'O';
+    public static void zufallZiele() {
+        for (int i = 0; i < spielfeld.length; i++) {
+            for (int j = 0; j < spielfeld[i].length; j++) {
+                if (spielfeld[i][j] != '/' && spielfeld[i][j] != '\\' && spielfeld[i][j] != '8') {
+                    if (Math.random() <= 0.3) {
+                        spielfeld[i][j] = 'O';
                     }
                 }
             }
         }
     }
-    public static void generierePfeil(char[][] arr, int y) {
-        arr[y/2][1] = '>';
+    public static void generierePfeil(int y) {
+        if (spielfeld[1][y/2] != '8') {
+            spielfeld[1][y/2] = '>';
+        } else {
+            spielfeld[1][Zufall.zufallGanz(1, spielfeld.length-2)] = '>';
+        }
+    }
+    public static void spiegelSimulation(int schritte, double drehwahrscheinlichkeit, int pfeilabstand) {
+
     }
     public static void main(String[] args) {
         SchischVisualizer sv = new SchischVisualizer();
-        char[][] spielfeld = blatt14.MultiArrays.createEmpty2DCharArray(40,40);
-        zufallSpiegel(spielfeld);
-        zufallZiele(spielfeld);
-        generierePfeil(spielfeld, 2);
-        print2DArraySpiegel(spielfeld);
+        zufallSpiegel();
+        zufallZiele();
+        generierePfeil(1);
         sv.step(spielfeld);
         sv.start();
     }
